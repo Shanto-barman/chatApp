bc2cd69f3641471b4bdcfca0d18570fb33fe505e
Shanta Barman <sha681nta@gmail.com>
2025-12-12 23:56:20 +0600

    profile

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
+import assets from '../assets/assets';
 const ProfilePage=()=> {
 
   const [selectedImg, setSelectedImg] = useState(null)
@@ -13,9 +14,12 @@ const ProfilePage=()=> {
         <form className='flex flex-col gap-5 p-10 flex-1'>
           <h3 className='text-lg'>Profile details</h3>
           <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
-            <input type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
-            <img src={""} alt="" />
+            <input onChange={()=>setSelectedImg(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
+            <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} alt="" className={`w-12 h-12 ${selectedImg && 'rounded-full'} `} />
+            upload profile image
           </label>
+          <input onChange={(e)=>setName} value={name}
+          type="text" required placeholder='You name' className='p-2 border border-gray-500 rounded-md focus:outlie none focus :ring-2' />
         </form>
         <img src="" alt="" />
       </div>
