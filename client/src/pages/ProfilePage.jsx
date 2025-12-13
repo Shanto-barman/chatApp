5146b7a82999ca20842cc18d1a2ad9e9d4128fbf
Shanta Barman <sha681nta@gmail.com>
2025-12-13 23:55:37 +0600

    profile

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -7,11 +7,16 @@ const ProfilePage=()=> {
   const navigate = useNavigate();
   const [name, setName] = useState("Martin Johnson")
   const [bio,setBio] = useState("Hi Everyone, I am Using QuickChat")
+  
+  const handleSubmit = async (e)=>{
+    e.preventDefult();
+    navigate('/')
 
+  }
   return (
     <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
       <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-certer justify-between max-sm:flex-col-reverse rounded-lg'>
-        <form className='flex flex-col gap-5 p-10 flex-1'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-5 p-10 flex-1'>
           <h3 className='text-lg'>Profile details</h3>
           <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
             <input onChange={()=>setSelectedImg(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
@@ -19,9 +24,13 @@ const ProfilePage=()=> {
             upload profile image
           </label>
           <input onChange={(e)=>setName} value={name}
-          type="text" required placeholder='You name' className='p-2 border border-gray-500 rounded-md focus:outlie none focus :ring-2' />
+          type="text" required placeholder='You name' className='p-2 border border-gray-500 rounded-md focus:outlie none focus :ring-2 focus:ring-violet-500'/>
+          <textarea onChange={(e)=>setBio} value={bio}
+          placeholder="write profile bio" required className='p-2 border border-gray-500 rounded-md focus:outlie none focus :ring-2 focus:ring-violet-500' rows={4}></textarea>
+
+          <button type='submit' className='bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>Save</button>
         </form>
-        <img src="" alt="" />
+        <img className='max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10' src={assets.logo_icon} alt="" />
       </div>
     </div>
   )
