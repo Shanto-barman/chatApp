34c6af349c397cf548ad338f3c060037b1526e19
Shanta Barman <sha681nta@gmail.com>
2025-12-14 23:45:34 +0600

    profile

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -9,17 +9,17 @@ const ProfilePage=()=> {
   const [bio,setBio] = useState("Hi Everyone, I am Using QuickChat")
   
   const handleSubmit = async (e)=>{
-    e.preventDefult();
+    e.preventDefault();
     navigate('/')
-
   }
+
   return (
     <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
       <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-certer justify-between max-sm:flex-col-reverse rounded-lg'>
         <form onSubmit={handleSubmit} className='flex flex-col gap-5 p-10 flex-1'>
           <h3 className='text-lg'>Profile details</h3>
           <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
-            <input onChange={()=>setSelectedImg(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
+            <input onChange={(e)=>setSelectedImg(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
             <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} alt="" className={`w-12 h-12 ${selectedImg && 'rounded-full'} `} />
             upload profile image
           </label>
