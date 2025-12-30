18170ed1632325af483125f25140ab658779efb3
Shanta Barman <sha681nta@gmail.com>
2025-12-30 23:49:40 +0600

    update

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,13 +1,29 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
-  senderId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
-  receiverId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
-  text: { type: String, },
-  imgage: { type: String, },
-  seen: {type: Boolean, default: false}
-}, {timestamps:true});
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  receiverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  text: {
+    type: String,
+    default: ""
+  },
+  image: {         
+    type: String,
+    default: null
+  },
+  seen: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
 
-const Message =mongoose.model("Message", messageSchema);
-
-export default Message; 
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
